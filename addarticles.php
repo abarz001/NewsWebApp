f51a401aef87f3a384e7b199eb31417f12843379
Aran Barzanji <42762069+abarz001@users.noreply.github.com>
2021-11-02 18:47:44 -0400

    Add files via upload

---
+++ addarticles.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+//Check if a list has been pasted
+if (isset($_POST['articlesList']) && strlen(trim($_POST['articlesList']))){
+require 'parse_original_articles.php';
+}
+?>
+
+
+<html>
+
+<head>
+    <title>(Admin) Add (original) fake news articles to database</title>
+</head>
+
+<body>
+    <div class="myform">
+    <form action="" method="post" name="frmSnopes" id="frmSnopes">
+        <table border="0" align="center" cellpadding="2" cellspacing="2">
+            <tr>
+                <td>Paste list of fake news articles, separated by new line.</td>
+				<td>Paste list of Snopes fact-check articles, separated by new line.</td>
+			</tr>
+			<tr>
+                <td><textarea rows="25" cols="75" name="articlesList" id="articlesList"></textarea></td>
+		                 <td><textarea rows="25" cols="75" name="snopesList" id="snopesList"></textarea></td>
+            </tr>
+			<tr>
+			<td><input name="btnAddSnopes" type="submit" id="btnAddSnopes" value="Add to database" class="buttons"></td>
+			</tr>
+        </table>
+    </form>
+    </div>
+</body>
+
+</html>
---
+++ parse_original_articles.php
@@ -0,0 +1,42 @@
+<?php
+   require_once 'simple_html_dom.php';
+   $snopesArray = explode("\r\n", $_POST['snopesList']);
+   $count = 0;
+   $fileCount = 1;
+   foreach(explode("\r\n", $_POST['articlesList']) as $article) {
+	echo "Snopes fact check URL: $snopesArray[$count]";
+	$snopesArticle = $snopesArray[$count];
+    echo "Original fake news article: $article<br>";
+	$html = file_get_html($article);
+	$url = $article;
+	echo $html->find('title',0)->plaintext . '<br><br>';
+	$title = $html->find('title',0)->plaintext;
+	$title = str_replace('\'', '', $title);
+	echo $html->find('body',0)->plaintext . '<br><br>';
+	$body = $html->find('body',0)->plaintext;
+	$body = str_replace('\'', '', $body);
+/* 	foreach($html->find('img') as $element)
+	{
+		echo $imgURL = "$element->src";
+		echo '<br>';
+	} */
+	
+	//Save the body for TextRank analysis
+	file_put_contents('./files/fake_news_' . $fileCount . '.txt', $body);
+	
+	//Do the insert into the articles table
+	$server = "localhost";
+    $sqlUser = "myadmin";
+    $sqlPass = "myadminpass";
+	$db = "PROJECT";
+	$conn = new mysqli($server, $sqlUser, $sqlPass, $db);
+    $articlesTable = "articles";
+	$insertStatement = "INSERT INTO $articlesTable (Title, URL, Body, Snopes_URL)
+						VALUES ('$title', '$url', '$body', '$snopesArticle')";
+	$query_result = $conn->query($insertStatement)
+                or die("SQL Query ERROR. There was an error inserting into the articles database:" . $insertStatement . $conn->connect_error);
+	$count++;
+	$fileCount++;
+	echo '<br>-----------------------------------------------------------------------------------<br>';
+	}
+?>
