5592de09152487b5642d8beeac04e97c07d0ffc7
Aran Barzanji <42762069+abarz001@users.noreply.github.com>
2021-11-03 14:21:45 -0400

    Add files via upload

--- retrieve_results.php
+++ retrieve_results.php
@@ -22,7 +22,7 @@ if ($results['hits']['total'] > 0){
 
 echo 'Total number of results: ' . count($results) . '<br><br>';
 foreach($results as $r){
-	echo '<pre>';
+    echo '<br>';
 	echo "Article #"; //debugging
 	print_r($r['_id']);
 	echo ": <a href=./searchresults.php?search=$searchTerm&article=";
@@ -38,14 +38,43 @@ if (isset($_GET['article'])){
 	echo 'You selected article number ' . $_GET['article'];
 	require 'grab_original_articles.php';
 		if (grabOriginalArticleBody($_GET['article'])){
+			echo "<link href=\"./css/bootstrap.min.css\" rel=\"stylesheet\">
+<script src=\"./js/jquery-3.6.0.min.js\"></script>
+<script src=\"./js/bootstrap.bundle.min.js\"></script>
+<script src=\"./js/load_tabs.js\"></script>";
 			echo "<body>
 				 <div class=\"splitpanel\"><form action=\"\" method=\"post\" name=\"frmProfile\" id=\"frmProfile\">
-							<table width=\"650\" border=\"1\" align=\"center\" cellpadding=\"3\" cellspacing=\"3\">
+							<table width=\"1500\" border=\"1\" align=\"center\" cellpadding=\"3\" cellspacing=\"3\">
 							<th>";
+							echo "<div class=\"overflow-auto\">";
 							echo grabOriginalArticleBody($_GET['article']);
-							echo "</th>
+							echo "</div></th>
 							<th>
-							Panel 2
+							
+	<div class=\"rightPanel\">
+    <ul class=\"nav nav-pills\" id=\"tabList\">
+        <li class=\"nav-item\">
+            <a href=\"#tab1\" class=\"nav-link active\">Semantic Search Articles</a>
+        </li>
+        <li class=\"nav-item\">
+            <a href=\"#tab2\" class=\"nav-link\">Snopes Fact-Check</a>
+        </li>
+        <li class=\"nav-item\">
+            <a href=\"#tab3\" class=\"nav-link\">Survey</a>
+        </li>
+    </ul>
+    <div class=\"tab-content\">
+        <div class=\"tab-pane fade show active\" id=\"tab1\">
+            <p>Tab 1 content</p>
+        </div>
+        <div class=\"tab-pane fade\" id=\"tab2\">
+            <p>Tab 2 content</p>
+        </div>
+        <div class=\"tab-pane fade\" id=\"tab3\">
+            <p>Tab 3 content</p>
+        </div>
+    </div>
+</div>
 							</th>
 					</table></form></div>
 					</body>";
@@ -55,3 +84,14 @@ if (isset($_GET['article'])){
 
 ?>
 
+<style>
+.overflow-auto{
+	max-height: 500px;
+	max-width: 800px;
+}
+.rightPanel{
+	max-height: 500px;
+	max-width: 600px;
+}
+
+</style>
