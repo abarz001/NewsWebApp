e2e058c7cc1c1a557e7e39ec8ffb214a6e61770c
Aran Barzanji <42762069+abarz001@users.noreply.github.com>
2021-11-07 16:13:44 -0500

    Add files via upload

--- files/generateKeywords.sh
+++ files/generateKeywords.sh
@@ -1,6 +1,7 @@
 sudo mkdir keywords
 COUNTER=1
-for file in /var/www/html/files/fake_news_*
+readarray -d '' files < <(printf '%s\0' /var/www/html/files/fake_news_* | sort -zV)
+for file in "${files[@]}";
 do
 textrank -t $file -s 1 -w 5 > ./keywords/keywords_for_$COUNTER.txt
 let COUNTER++
