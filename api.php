bcac61ae1715f3fe8c3a3245c402b7d7e978b4d6
Aran Barzanji <42762069+abarz001@users.noreply.github.com>
2021-12-02 21:10:08 -0500

    API page updates

--- api.php
+++ api.php
@@ -1,29 +1,33 @@
 <?php
-//Connect to db
-$server = "localhost";
-$sqlUser = "myadmin";
-$sqlPass = "myadminpass";
-$db = "PROJECT";
-$conn = new mysqli($server, $sqlUser, $sqlPass, $db);
-$table = "articles";
-$sqlStatement = "SELECT * FROM $table";
+header("Content-Type: JSON");
+require 'vendor/autoload.php';
+$searchTerm = htmlspecialchars($_GET['query']);
+$articleLimit = htmlspecialchars($_GET['n']);
+$client = Elasticsearch\ClientBuilder::create()->build();
 
-//Run the SQL statement, return false if error.
-$query_result = $conn->query($sqlStatement);
-if (!$query_result) {
-    echo $sqlStatement;
-    echo "<br>Query error.";
-    die;
+$params = [
+    'index' => 'fake_articles',
+    'body'  => [
+        'query' => [
+            'fuzzy' => ['title' => $searchTerm]
+		]
+    ]
+];
+
+$results = $client->search($params);
+$json_preliminary_result = array();
+if ($results['hits']['total'] > 0){
+	$results = $results['hits']['hits'];
 }
-else {
-    
-    $result = array();
-    $i = 0;
-    while ($res = mysqli_fetch_assoc($query_result)){
-        $result[$i]['title'] = $res['Title'];
+$i = 0;
+foreach($results as $r){
+    if ($i < $articleLimit){
+        $json_preliminary_result['Time'] = date('Y-m-d H:i:s');
+        $json_preliminary_result[$i]['Rank'] = $i+1;
+        $json_preliminary_result[$i]['Article_ID'] = $r['_id'];
+        $json_preliminary_result[$i]['Article_Title'] = $r['_source']['title'];
         $i++;
     }
-    header("Content-Type: JSON");
-    echo json_encode($result, JSON_PRETTY_PRINT);
 }
+echo json_encode($json_preliminary_result, JSON_PRETTY_PRINT);
 ?>
